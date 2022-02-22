3d9b180ae0d994282c41e106ae052a976b1744b7
guilhermepires <gguilhermepires@gmail.com>
2022-02-22 04:43:49 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication5/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication5
+{
+    public class UserDto
+    {
+
+        public string UserName { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
